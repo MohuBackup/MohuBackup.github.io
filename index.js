d85580c1bc0d3b66cc584f5fc312790aa8300d61
MohuBackup <backup@mohu.club>
2019-01-04 16:04:23 +0800

    优化临时首页，显著提升页面加载速度

--- index.js
+++ index.js
@@ -7,55 +7,57 @@
  * @param {*} data 
  */
 function createItemDiv(type, data) {
-    var itemDiv = document.createElement("div")
-    itemDiv.classList.add("aw-item")
+    return function () {
+        var itemDiv = document.createElement("div")
+        itemDiv.classList.add("aw-item")
 
-    var avatarA = document.createElement("a")
-    avatarA.classList.add("aw-user-name", "hidden-xs")
-    var avatarImg = document.createElement("img")
-    avatarImg.src = data.authorAvatar || "/static/common/avatar-mid-img.png"
-    avatarA.appendChild(avatarImg)
+        var avatarA = document.createElement("a")
+        avatarA.classList.add("aw-user-name", "hidden-xs")
+        var avatarImg = document.createElement("img")
+        avatarImg.src = data.authorAvatar || "/static/common/avatar-mid-img.png"
+        avatarA.appendChild(avatarImg)
 
-    var contentDiv = document.createElement("div")
-    contentDiv.classList.add("aw-question-content")
+        var contentDiv = document.createElement("div")
+        contentDiv.classList.add("aw-question-content")
 
-    var titleH4 = document.createElement("h4")
-    var titleA = document.createElement("a")
-    titleA.href = "/" + type + "/" + data.id
-    titleA.text = data.title
-    titleH4.appendChild(titleA)
+        var titleH4 = document.createElement("h4")
+        var titleA = document.createElement("a")
+        titleA.href = "/" + type + "/" + data.id
+        titleA.text = data.title
+        titleH4.appendChild(titleA)
 
-    var contentP = document.createElement("p")
+        var contentP = document.createElement("p")
 
-    var tagA = document.createElement("a")
-    tagA.classList.add("aw-question-tags")
-    tagA.text = "默认分类"
+        var tagA = document.createElement("a")
+        tagA.classList.add("aw-question-tags")
+        tagA.text = "默认分类"
 
-    var userNameA = document.createElement("a")
-    userNameA.classList.add("aw-user-name")
+        var userNameA = document.createElement("a")
+        userNameA.classList.add("aw-user-name")
 
-    var contentSpan = document.createElement("span")
+        var contentSpan = document.createElement("span")
 
-    if (type == "question") {
-        if (data.lastAnswer) {
-            userNameA.text = data.lastAnswer.authorUserName || "匿名用户"
-            contentSpan.innerText = " 回复了问题 • " + data.concerns + " 人关注 • " + data.answersCount + " 个回复 • " + data.views + " 次浏览 • " + data.lastAnswer.publishDate
-        } else {  // 没有回答
+        if (type == "question") {
+            if (data.lastAnswer) {
+                userNameA.text = data.lastAnswer.authorUserName || "匿名用户"
+                contentSpan.innerText = " 回复了问题 • " + data.concerns + " 人关注 • " + data.answersCount + " 个回复 • " + data.views + " 次浏览 • " + data.lastAnswer.publishDate
+            } else {  // 没有回答
+                userNameA.text = data.authorUserName || "匿名用户"
+                contentSpan.innerText = " 发起了问题  • " + data.concerns + " 人关注 • " + data.answersCount + " 个回复 • " + data.views + " 次浏览 • " + data.publishDate
+            }
+        } else {
             userNameA.text = data.authorUserName || "匿名用户"
-            contentSpan.innerText = " 发起了问题  • " + data.concerns + " 人关注 • " + data.answersCount + " 个回复 • " + data.views + " 次浏览 • " + data.publishDate
+            contentSpan.innerText = " 发表了文章 • " + data.voters + " 人点赞 • " + data.commentsCount + " 条评论 • " + data.publishDate
         }
-    } else {
-        userNameA.text = data.authorUserName || "匿名用户"
-        contentSpan.innerText = " 发表了文章 • " + data.voters + " 人点赞 • " + data.commentsCount + " 条评论 • " + data.publishDate
+
+        contentP.append(tagA, " • ", userNameA, contentSpan)
+
+        contentDiv.append(titleH4, contentP)
+
+        itemDiv.append(avatarA, contentDiv)
+
+        return itemDiv
     }
-
-    contentP.append(tagA, " • ", userNameA, contentSpan)
-
-    contentDiv.append(titleH4, contentP)
-
-    itemDiv.append(avatarA, contentDiv)
-
-    return itemDiv
 }
 
 /**
@@ -122,7 +124,7 @@ var pathname = top.location.pathname
 var categories = document.querySelectorAll(".category li")
 var pageTitle = document.querySelector("#page-title")
 
-/** @type {HTMLDivElement[]} */
+/** @type {( () => HTMLDivElement )[]} */
 var items = []
 
 /** @type {Object[]} */
@@ -162,7 +164,9 @@ var main = function () {
     var page = pageHashMatch ? +pageHashMatch[1] : 1
     var n = 10
 
-    var output = items.slice(n * (page - 1), n * page)
+    var output = items.slice(n * (page - 1), n * page).map(function (item) {
+        return item()
+    })
 
     var newEl = document.createElement("div")
     newEl.classList.add("aw-common-list")
